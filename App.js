b29cbf471189a25ee5266e19b985e6f097370dd7
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-03-24 15:23:23 -0300

    funcionou em partes

--- App.js
+++ App.js
@@ -1,28 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
 import { React, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
   const[vlEtanol, setVlEtanol] = useState();
   const[vlGasolina, setVlGasolina] = useState();
   const[vlResultado, setVlResultado] = useState();
+  const[txtMensagem, setTxtMensagem] = useState();
 
   function verificaResultado(){
     setVlResultado (parseFloat(vlEtanol) / parseFloat(vlGasolina));
+    if(vlResultado > 0.7){
+      setTxtMensagem("É mais vantajoso abastecer com gasolina.");
+    }else if(vlResultado <= 0.7){
+      setTxtMensagem("É mais vantajoso abastecer com álcool.");
+    }
   }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.texto}>ETANOL OU GASOLINA?</Text>
+      <Text style={styles.titulo}>ETANOL OU GASOLINA?</Text>
       <Text style={styles.subtexto}>Sabemos que, no dia de hoje, abastecer um veículo é uma tarefa que exige uma certa informação sobre preços, pois a escolha certa do tipo de combustível pode ser muitas vezes essencial para que possamos utilizar o veículo com tranquilidade com o que temos em mãos. É por isso então que vamos calcular o que vale mais a pena: Etanol ou Gasolina!</Text>
+      <View style={styles.bloco}>
+        <Text style={styles.textoBloco}>Valor do Etanol</Text>
+          <TextInput 
+            style={styles.input}
+            keyboardType="numeric"
+            value={vlEtanol}
+            onChangeText={(texto)=>setVlEtanol(texto)}
+          />
+      </View>
       <View style={styles.bloco}>
         <Text style={styles.textoBloco}>Valor da Gasolina</Text>
           <TextInput 
             style={styles.input}
             keyboardType="numeric"
-            value={valor1}
-            onChangeText={(texto)=>setValor1(texto)}
+            value={vlGasolina}
+            onChangeText={(texto)=>setVlGasolina(texto)}
           />
+      </View>
+      <View style={styles.bloco}>
+        <TouchableOpacity 
+          style={styles.botao}
+          onPress={verificaResultado}
+          >
+            <Text style={styles.textoBotao}>Verificar</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.bloco}>
+            <Text style={styles.textoBloco}>Resultado: {txtMensagem}</Text>
       </View>
     </View>
   );
@@ -35,20 +60,35 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  texto:{
-    fontSize: 45,
-    color: '#DB9344',
+  titulo:{
+    fontSize: 35,
+    color: '#FFF',
+    backgroundColor:'#9DD5FA',
+    width: '100%',
+    textAlign: 'center',
+    marginBottom: -10,
+    marginTop: -90,
+    paddingTop: 20,
+    paddingBottom: 20,
     fontFamily: 'fantasy'
   },
   subtexto:{
-    fontSize: 45,
+    fontSize: 20,
+    backgroundColor:'#9DD5FA',
+    paddingLeft: '10%',
+    paddingRight: '10%',
+    paddingBottom: 20,
     color: '#000',
-    fontFamily: 'fantasy'
+    textAlign: 'justify',
+    fontFamily: 'sans-serif'
   },
   textoBloco:{
     fontSize:30,
-    color: '#37259C',
-    fontFamily: 'monospace'
+    color: '#DE8152',
+    fontFamily: 'sans-serif',
+    paddingLeft: '10%',
+    paddingRight: '10%',
+    paddingBottom: 20
   },
   input:{
     borderColor:'#000',
@@ -62,7 +102,7 @@ const styles = StyleSheet.create({
     marginTop:30
   },
   botao:{
-    backgroundColor:'#DB9344',
+    backgroundColor:'#9DD5FA',
     width:'80%',
     textAlign:'center'
   },
